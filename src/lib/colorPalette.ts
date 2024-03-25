f38f3c82918ed7171c3965e5f92d084fdacbb0bb
overloper <overloper0225@gmail.com>
2024-03-25 15:46:06 +0900

    feat: Add Color palette

---
+++ src/lib/colorPalette.ts
@@ -0,0 +1,11 @@
+interface ColorPalette {
+  mainColor1: string;
+  mainColor2: string;
+}
+
+const palette: ColorPalette = {
+  mainColor1: '#FFBB5C',
+  mainColor2: '#FF9B50',
+};
+
+export default palette;
